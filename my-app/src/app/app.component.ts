01ee4b71c3cbf648f338ac19b9e07b327b78282a
Matt Holloway <hollowmatt@gmail.com>
2024-05-10 08:43:12 -0400

    update angular version

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `
+    <h1>Welcome to {{title}}!</h1>
+
+    <router-outlet />
+  `,
+  styles: [],
 })
 export class AppComponent {
   title = 'my-app';
