7d7103a062e070623b002b2be6bb9a991626a3ac
Matt Holloway <hollowmatt@gmail.com>
2024-05-12 10:58:17 -0400

    update with ngIf

--- my-app/src/app/settings/settings.component.ts
+++ my-app/src/app/settings/settings.component.ts
@@ -1,13 +1,14 @@
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @Component({
   standalone: true,
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
 })
 
 export class SettingsComponent {
-  settingOne = "peanut butter";
+  shouldDisplay = true;
 }
