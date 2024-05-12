598da33e5a3cd271ee6f8a3f35ff5856387c875c
Matt Holloway <hollowmatt@gmail.com>
2024-05-12 10:09:41 -0400

    ngModel

--- my-app/src/app/settings/settings.component.ts
+++ my-app/src/app/settings/settings.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   standalone: true,
   selector: 'app-settings',
-  templateUrl: './settings.component.html'
+  templateUrl: './settings.component.html',
+  imports: [FormsModule],
 })
 
 export class SettingsComponent {
