a1655c95b7610e5777d98c5668e9ea55bb696e23
Matt Holloway <hollowmatt@gmail.com>
2024-05-12 09:39:36 -0400

    pass value

--- my-app/src/app/settings/settings.component.ts
+++ my-app/src/app/settings/settings.component.ts
@@ -6,4 +6,6 @@ import { Component } from "@angular/core";
   templateUrl: './settings.component.html'
 })
 
-export class SettingsComponent {}
+export class SettingsComponent {
+  settingOne = "peanut butter";
+}
